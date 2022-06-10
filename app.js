d8e312e9db2cf8c8efb804b6f14e21992c08a201
miguelbriceno <mabo544@gmail.com>
2022-06-10 18:27:29 -0500

    Initial design

--- app.js
+++ app.js
@@ -14,14 +14,9 @@ app.set("view engine", "ejs");
 //Root rute
 app.get("/", function(req, res) {
   console.log("Server runnig on port 3000".bgWhite.blue);
-  //res.send("Hello World"); //For testing
-  res.sendFile(__dirname + "/index.html"); // To recive from a Form
+  res.render(__dirname + "/index.ejs"); // To recive from a Form
 });
 
-//Sample page rute
-app.get("/sample", (req, res) => {
-  res.render("sample");
-});
 
 //app.post("/", function(req, res){
 //});
